4c2174b6e7022404f099c8dbd0637ec2c94bcba3
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-13 17:32:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,11 +47,11 @@ class SongsController < ApplicationController
       artist = Arist.find_by(artist_id: params[artist_id])
      if artist.nil?
       redirect_to artists_path, alert: "Artist not found."
-     else 
+     else
       @song = artist.songs.find_by(id: params[:id])
       redirect_to artist_songs_path(artist), alert: "Song not found."
     end
-  else  
+  else
     @song = Song.find(params[:id])
   end
 end
